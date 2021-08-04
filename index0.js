c7075e75f1754e19eb6b97bff2fe3ac28039ad9b
takuto1014 <76800704+takuto1014@users.noreply.github.com>
2021-08-04 15:10:35 +0900

    Update index0.js

--- index0.js
+++ index0.js
@@ -22,14 +22,14 @@ var link = localStorage.getItem('key');
 var result = link.split(";")
 var result_0 = result[0].split("=")
 //alert(result_0[1] + result[1])
-var img_1 = "img/"+result_0[1]+".png"
-var img_2 = "img/"+result[1]+".png"
-var img_3 = "img/"+result[2]+".png"
-var img_4 = "img/"+result[3]+".png"
-var img_5 = "img/"+result[4]+".png"
-var img_6 = "img/"+result[5]+".png"
-var img_7 = "img/"+result[6]+".png"
-var img_8 = "img/"+result[7]+".png"
+var img_1 = result_0[1]+".png"
+var img_2 = result[1]+".png"
+var img_3 = result[2]+".png"
+var img_4 = result[3]+".png"
+var img_5 = result[4]+".png"
+var img_6 = result[5]+".png"
+var img_7 = result[6]+".png"
+var img_8 = result[7]+".png"
 let img_01 = document.getElementById("image_place1");
 img_01.src = img_1;
 let img_02 = document.getElementById("image_place2");
