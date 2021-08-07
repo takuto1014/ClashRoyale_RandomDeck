9f8dcc7b40641b2badff5d4b8eed1eb096a52d11
takuto1014 <76800704+takuto1014@users.noreply.github.com>
2021-08-07 17:33:51 +0900

    Add files via upload

--- index0.js
+++ index0.js
@@ -3,53 +3,91 @@
         const textbox = document.getElementById("link")
         const value = textbox.value
         if(value.indexOf('https://link.clashroyale.com/deck/en?deck=2') != -1 || value.indexOf('https://link.clashroyale.com/deck/jp?deck=2') != -1){
-            localStorage.setItem('key', value);
+            var number = localStorage.length
+            localStorage.setItem('key'+number, value);
             location.reload();
         }else{
             alert("入力したリンクが間違っています")
         }
         
     }, false);
-
+    
     document.getElementById("link_button2").addEventListener("click", function () {
-        var link = localStorage.getItem('key');
-        location.href=link
-
-        
+        localStorage.clear()  
+        location.reload();      
     }, false);
 
-    var link = localStorage.getItem('key');
-    var result = link.split(";")
-    var result_0 = result[0].split("=")
-    var result_8 = result[7].split("&")
-    //alert(result_0[1] + result[1])
-    var img_1 = result_0[1]+".png"
-    var img_2 = result[1]+".png"
-    var img_3 = result[2]+".png"
-    var img_4 = result[3]+".png"
-    var img_5 = result[4]+".png"
-    var img_6 = result[5]+".png"
-    var img_7 = result[6]+".png"
-    var img_8 = result_8[0]+".png"
-    let img_01 = document.getElementById("image_place1");
-    img_01.src = img_1;
-    let img_02 = document.getElementById("image_place2");
-    img_02.src = img_2;
-    let img_03 = document.getElementById("image_place3");
-    img_03.src = img_3;
-    let img_04 = document.getElementById("image_place4");
-    img_04.src = img_4;
-    let img_05 = document.getElementById("image_place5");
-    img_05.src = img_5;
-    let img_06 = document.getElementById("image_place6");
-    img_06.src = img_6;
-    let img_07 = document.getElementById("image_place7");
-    img_07.src = img_7;
-    let img_08 = document.getElementById("image_place8");
-    img_08.src = img_8;
+    var number = localStorage.length
+    for(i=1;i<number;i++){
+        //リンクを設定
+        document.getElementById("link"+i).addEventListener("click", function () {
+            var link = localStorage.getItem('key'+i);
+            location.href=link
+        }, false);
+        
+        //画像を表示
+        var link = localStorage.getItem('key'+i);
+        var result = link.split(";")
+        var result_0 = result[0].split("=")
+        var result_8 = result[7].split("&")
+        //alert(result_0[1] + result[1])
+        var img_1 = result_0[1]+".png"
+        var img_2 = result[1]+".png"
+        var img_3 = result[2]+".png"
+        var img_4 = result[3]+".png"
+        var img_5 = result[4]+".png"
+        var img_6 = result[5]+".png"
+        var img_7 = result[6]+".png"
+        var img_8 = result_8[0]+".png"
+        let img_01 = document.getElementById("image_place"+i+"-1");
+        img_01.src = img_1;
+        let img_02 = document.getElementById("image_place"+i+"-2");
+        img_02.src = img_2;
+        let img_03 = document.getElementById("image_place"+i+"-3");
+        img_03.src = img_3;
+        let img_04 = document.getElementById("image_place"+i+"-4");
+        img_04.src = img_4;
+        let img_05 = document.getElementById("image_place"+i+"-5");
+        img_05.src = img_5;
+        let img_06 = document.getElementById("image_place"+i+"-6");
+        img_06.src = img_6;
+        let img_07 = document.getElementById("image_place"+i+"-7");
+        img_07.src = img_7;
+        let img_08 = document.getElementById("image_place"+i+"-8");
+        img_08.src = img_8;
+    }
 
 
 
 
 
-
+    for(i=0;i<links1.length;i++){
+        link1 = links1[i]
+        link_split = link1.split(";")
+        link_split2 = link_split[0].split("=")
+        card1 = link_split2[1]+".png";
+        card2 = link_split[1]+".png";
+        card3 = link_split[2]+".png";
+        card4 = link_split[3]+".png";
+        card5 = link_split[4]+".png";
+        card6 = link_split[5]+".png";
+        card7 = link_split[6]+".png";
+        card8 = link_split[7]+".png";
+    
+        let img_1 = document.getElementById("image_place1-"+i+"-1");
+        img_1.src = card1;
+        let img_2 = document.getElementById("image_place1-"+i+"-2");
+        img_2.src = card2;
+        let img_3 = document.getElementById("image_place1-"+i+"-3");
+        img_3.src = card3;
+        let img_4 = document.getElementById("image_place1-"+i+"-4");
+        img_4.src = card4;
+        let img_5 = document.getElementById("image_place1-"+i+"-5");
+        img_5.src = card5;
+        let img_6 = document.getElementById("image_place1-"+i+"-6");
+        img_6.src = card6;
+        let img_7 = document.getElementById("image_place1-"+i+"-7");
+        img_7.src = card7;
+        let img_8 = document.getElementById("image_place1-"+i+"-8");
+        img_8.src = card8;
+    }    
--- index9.js
+++ index9.js
@@ -1,3 +1,4 @@
+//【デッキリンク】
 //レトロロワイヤルチャレンジ
 links1 = ["https://link.clashroyale.com/deck/en?deck=28000001;26000015;26000006;26000008;26000037;26000029;26000032;28000008","https://link.clashroyale.com/deck/en?deck=28000001;26000027;28000000;26000003;28000010;26000037;26000012;26000007","https://link.clashroyale.com/deck/en?deck=28000001;26000006;26000013;26000037;26000029;26000032;26000005;26000010","https://link.clashroyale.com/deck/en?deck=28000004;26000030;26000026;28000003;26000010;27000006;28000011;26000011","https://link.clashroyale.com/deck/en?deck=26000015;26000006;26000034;28000005;26000037;26000035;28000011;28000012","https://link.clashroyale.com/deck/en?deck=26000027;27000007;26000003;26000018;26000022;28000002;26000033;26000017","https://link.clashroyale.com/deck/en?deck=26000027;26000042;28000000;26000003;26000039;26000032;26000016;28000008","https://link.clashroyale.com/deck/en?deck=26000015;26000006;28000005;26000023;26000035;26000004;28000012;28000008","https://link.clashroyale.com/deck/en?deck=26000001;28000000;26000030;26000000;26000010;27000006;28000011;27000008","https://link.clashroyale.com/deck/en?deck=28000004;26000030;26000000;26000026;28000003;26000012;27000006;28000011","https://link.clashroyale.com/deck/en?deck=26000023;26000000;28000003;26000010;27000006;28000011;28000012;27000008","https://link.clashroyale.com/deck/en?deck=27000000;28000000;26000021;26000038;26000030;26000014;26000010;28000011","https://link.clashroyale.com/deck/en?deck=26000027;26000042;27000010;26000030;28000007;26000039;26000024;28000011"]
 document.getElementById("link1-0").addEventListener("click", function () {
@@ -36,10 +37,64 @@ document.getElementById("link1-10").addEventListener("click", function () {
 document.getElementById("link1-11").addEventListener("click", function () {
     location.href=links1[11]
 }, false); 
+document.getElementById("link1-12").addEventListener("click", function () {
+    location.href=links1[12]
+}, false);
 
+//新参者チャレンジ
+links2 = ["https://link.clashroyale.com/deck/en?deck=26000046;28000015;26000042;26000037;26000055;26000083;28000009;26000051","https://link.clashroyale.com/deck/en?deck=26000046;28000015;26000054;26000027;26000042;27000013;26000083;28000009","https://link.clashroyale.com/deck/en?deck=28000015;26000027;28000014;26000085;27000010;26000037;26000083;28000012","https://link.clashroyale.com/deck/en?deck=26000046;28000015;26000042;28000017;26000037;26000055;28000009;26000051","https://link.clashroyale.com/deck/en?deck=26000027;28000014;26000084;26000061;26000039;26000083;26000024;28000011","https://link.clashroyale.com/deck/en?deck=26000046;28000015;26000036;26000054;26000042;28000016;26000062;26000050","https://link.clashroyale.com/deck/en?deck=26000068;26000063;26000085;26000043;27000010;28000016;26000083;28000012","https://link.clashroyale.com/deck/en?deck=26000046;28000015;28000017;26000038;26000037;26000035;26000062;26000051","https://link.clashroyale.com/deck/en?deck=28000015;26000068;26000063;26000067;28000016;26000037;26000048;28000012","https://link.clashroyale.com/deck/en?deck=26000027;26000084;26000061;26000039;26000083;28000009;26000024;28000011","https://link.clashroyale.com/deck/en?deck=26000046;26000054;26000027;26000042;27000013;28000016;26000083;28000011","https://link.clashroyale.com/deck/en?deck=28000015;26000027;26000085;26000061;27000010;26000037;26000083;28000012","https://link.clashroyale.com/deck/en?deck=26000046;26000036;26000042;26000062;26000055;28000009;26000050;28000011","https://link.clashroyale.com/deck/en?deck=26000068;26000063;26000043;26000067;28000016;26000037;26000048;28000012"]
+document.getElementById("link2-0").addEventListener("click", function () {
+    location.href=links2[0]
+}, false); 
+document.getElementById("link2-1").addEventListener("click", function () {
+    location.href=links2[1]
+}, false); 
+document.getElementById("link2-2").addEventListener("click", function () {
+    location.href=links2[2]
+}, false); 
+document.getElementById("link2-3").addEventListener("click", function () {
+    location.href=links2[3]
+}, false); 
+document.getElementById("link2-4").addEventListener("click", function () {
+    location.href=links2[4]
+}, false); 
+document.getElementById("link2-5").addEventListener("click", function () {
+    location.href=links2[5]
+}, false); 
+document.getElementById("link2-6").addEventListener("click", function () {
+    location.href=links2[6]
+}, false); 
+document.getElementById("link2-7").addEventListener("click", function () {
+    location.href=links2[7]
+}, false); 
+document.getElementById("link2-8").addEventListener("click", function () {
+    location.href=links2[8]
+}, false); 
+document.getElementById("link2-9").addEventListener("click", function () {
+    location.href=links2[9]
+}, false); 
+document.getElementById("link2-10").addEventListener("click", function () {
+    location.href=links2[10]
+}, false); 
+document.getElementById("link2-11").addEventListener("click", function () {
+    location.href=links2[11]
+}, false); 
+document.getElementById("link2-12").addEventListener("click", function () {
+    location.href=links2[12]
+}, false); 
+document.getElementById("link2-13").addEventListener("click", function () {
+    location.href=links2[13]   
+}, false); 
+
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+//【画像表示】
+//レトロロワイヤルチャレンジ
 for(i=0;i<links1.length;i++){
-    link = links1[i]
-    link_split = link.split(";")
+    link1 = links1[i]
+    link_split = link1.split(";")
     link_split2 = link_split[0].split("=")
     card1 = link_split2[1]+".png";
     card2 = link_split[1]+".png";
@@ -66,8 +121,38 @@ for(i=0;i<links1.length;i++){
     img_7.src = card7;
     let img_8 = document.getElementById("image_place1-"+i+"-8");
     img_8.src = card8;
-    //console.log(card1)
-
-    
 }
+
+//新参者チャレンジ
+for(i=0;i<links2.length;i++){
+    link2 = links2[i]
+    link_split = link2.split(";")
+    link_split2 = link_split[0].split("=")
+    card1 = link_split2[1]+".png";
+    card2 = link_split[1]+".png";
+    card3 = link_split[2]+".png";
+    card4 = link_split[3]+".png";
+    card5 = link_split[4]+".png";
+    card6 = link_split[5]+".png";
+    card7 = link_split[6]+".png";
+    card8 = link_split[7]+".png";
+
+    let img2_1 = document.getElementById("image_place2-"+i+"-1");
+    img2_1.src = card1;
+    let img2_2 = document.getElementById("image_place2-"+i+"-2");
+    img2_2.src = card2;
+    let img2_3 = document.getElementById("image_place2-"+i+"-3");
+    img2_3.src = card3;
+    let img2_4 = document.getElementById("image_place2-"+i+"-4");
+    img2_4.src = card4;
+    let img2_5 = document.getElementById("image_place2-"+i+"-5");
+    img2_5.src = card5;
+    let img2_6 = document.getElementById("image_place2-"+i+"-6");
+    img2_6.src = card6;
+    let img2_7 = document.getElementById("image_place2-"+i+"-7");
+    img2_7.src = card7;
+    let img2_8 = document.getElementById("image_place2-"+i+"-8");
+    img2_8.src = card8;    
+}
+
 
